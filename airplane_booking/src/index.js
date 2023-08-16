ac2a04d312e93bfb4ec0c32b5a34ecb0f6ff443c
huyhuynhC0223G1 <huynhduchuy403@gmail.com>
2023-08-16 08:39:22 +0700

    huy merge lan 5

--- airplane_booking/src/index.js
+++ airplane_booking/src/index.js
@@ -24,7 +24,7 @@ import CustomerDetails from "./component/CustomerDetails";
 import DetailTicket from "./component/ticket/DetailTicket";
 import InfoPassenger from "./component/ticket/InfoPassenger";
 import EmployeeList from "./component/Employee/EmployeeList";
-import CreateEmployee from "./component/Employee/CreateEmployee";
+// import CreateEmployee from "./component/Employee/CreateEmployee";
 import ListPost from "./component/post/ListPost";
 // import SearchTicketPage from "./components/searchTickets_KietNT/SearchTicketPage";
 // import SearchResultPage from "./components/searchTickets_KietNT/SearchResultPage";
@@ -53,7 +53,7 @@ root.render(
                 <Route path='/list/:data' element={<ListRouter />} />
                 <Route path="/detail-ticket/:data" element={<DetailTicket />} />
                 <Route path="/info-passenger/:data" element={<InfoPassenger/>} />
-                {/*<Route path="/employee" element={<EmployeeList/>} />*/}
+                <Route path="/employee" element={<EmployeeList/>} />
                 {/*<Route path='/employee/create' element={<CreateEmployee />} />*/}
                 <Route path='/listPost' element={<ListPost />} />
 
