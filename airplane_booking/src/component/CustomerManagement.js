ad44589137564998319d12054faef4df2e6ad62d
TranDinhNgocSang <sangdolphin98@gmail.com>
2023-08-21 00:17:06 +0700

    SangTDN commit truoc khi merge len nhanh dev 21/8/2023

--- airplane_booking/src/component/CustomerManagement.js
+++ airplane_booking/src/component/CustomerManagement.js
@@ -142,8 +142,6 @@ export default function CustomerManagement() {
 
             <div className="background-customer">
                 <meta charSet="UTF-8"/>
-                <title>Quản lí khách hàng</title>
-
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
                       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
                       crossOrigin="anonymous"/>
