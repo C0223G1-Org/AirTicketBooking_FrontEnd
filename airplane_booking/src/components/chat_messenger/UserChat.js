c3a3403f06b2836893eae2e57f2cc339ca77b16e
Nguyen Tuan Kiet <github.com>
2023-08-20 13:41:38 +0700

    KietNT update FE 20/8

--- airplane_booking/src/components/chat_messenger/UserChat.js
+++ airplane_booking/src/components/chat_messenger/UserChat.js
@@ -9,10 +9,10 @@ const UserChat = () => {
   const [chatId, setChatId] = useState(null);
   const [messages, setMessages] = useState([]);
   const [adminMessages, setAdminMessages] = useState([]);
-  const [chatForm, setChatForm] = useState(false)
+  const [chatForm, setChatForm] = useState(false);
   const [showChatbox, setShowChatbox] = useState(false);
   const [showButton, setShowbutton] = useState(false);
-  const [hasSentStartMessage, setHasSentStartMessage] = useState(false);
+  const [isFirstMessage, setIsFirstMessage] = useState(true);
   useEffect(() => {
     if (chatId) {
       const messagesRef = ref(database, `chats/${chatId}/messages`);
@@ -30,15 +30,26 @@ const UserChat = () => {
       setAdminMessages(messages);
     });
   }, [chatId]);
+
   const handleStartChat = async () => {
     if (username.trim() === "") return;
     // Tạo room chat riêng cho người dùng
     const chatRef = push(ref(database, "chats"));
     const currentTime = new Date();
+
     const newChatId = chatRef.key;
     // Lưu tên người dùng và chat ID vào database
     push(ref(database, `chats/${newChatId}/user`), newChatId);
     push(ref(database, `users/${username}`), newChatId);
+    const userRef = ref(database, `users/${username}`);
+    set(userRef, {
+      chatId: newChatId,
+      timestamp: currentTime.toLocaleDateString("vi-VN", {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+    });
+
     const startMessage = {
       sender: "admin",
       content: `Chào ${username}, Chúc bạn một ngày tốt lành. Tôi là trợ lý ảo của Codegym Airline, Bạn cần chúng tôi trợ giúp điều gì ?`,
@@ -50,9 +61,10 @@ const UserChat = () => {
     push(ref(database, `chats/${username}/messages`), startMessage);
     setChatId(username);
     setChatStarted(true);
-    setChatForm(!chatForm)
+    setChatForm(!chatForm);
   };
   console.log(showChatbox);
+
   const handleSendMessage = () => {
     if (message.trim() === "") return;
     const currentTime = new Date();
@@ -66,7 +78,8 @@ const UserChat = () => {
     };
     // Gửi tin nhắn mới lên database
     push(ref(database, `chats/${chatId}/messages`), newMessage);
-    if (!hasSentStartMessage) {
+    if (isFirstMessage) {
+      // Gửi tin nhắn bắt đầu từ phía admin
       const startMessage = {
         sender: "admin",
         content: `Cảm ơn bạn đã liên hệ với chúng tôi, Tư vấn viên của Codegym Airline sẽ phản hồi bạn trong thời gian sớm nhất!`,
@@ -76,14 +89,15 @@ const UserChat = () => {
         }),
       };
       push(ref(database, `chats/${username}/messages`), startMessage);
-      setHasSentStartMessage(true);
+      // Cập nhật biến trạng thái isFirstMessage
+      setIsFirstMessage(false);
     }
     setMessage("");
   };
   return (
     <>
       <div>
-        {chatForm &&
+        {chatForm && (
           <div
             className="form-popup"
             style={{
@@ -91,27 +105,31 @@ const UserChat = () => {
               display: chatStarted ? "block" : "none",
             }}
           >
-            <h2 className="h22" >
-              CodeGym AirLine
-            </h2>
+            <h2 className="h22">CodeGym AirLine</h2>
             <div className="row clearfix">
-              <div 
-              id="style-7"
+              <div
+                id="style-7"
                 // className="col-lg-12"
                 style={{ overflowY: "scroll", height: "280px" }}
               >
-
                 <div className="chat-history">
                   <ul className="m-b-0">
                     {messages.map((message, index) => (
                       <li
                         key={index}
-                        className={`clearfix ${message.sender === "admin"
-                          ? "seft-message-user"
-                          : "other-message-admin"
-                          }`}
+                        className={`clearfix ${
+                          message.sender === "admin"
+                            ? "seft-message-user"
+                            : "other-message-admin"
+                        }`}
                       >
-                        <div  className="message" >{message.content} <br /><span style={{fontSize:'10px',float:'left'}}> {message.timestamp}</span></div>
+                        <div className="message">
+                          {message.content} <br />
+                          <span style={{ fontSize: "10px", float: "left" }}>
+                            {" "}
+                            {message.timestamp}
+                          </span>
+                        </div>
                       </li>
                     ))}
                     {adminMessages.map((msg, index) => (
@@ -125,7 +143,7 @@ const UserChat = () => {
                 </div>
               </div>
             </div>
-            <div className="row reply" style={{ height: '50px' }}>
+            <div className="row reply" style={{ height: "50px" }}>
               <div className="col-sm-10 col-xs-10 reply-main">
                 <input
                   onKeyDown={(event) => {
@@ -147,65 +165,105 @@ const UserChat = () => {
                 onClick={handleSendMessage}
               >
                 {" "}
-                <svg style={{ top: '35px', left: '10px' }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" data-reactid="1036">
+                <svg
+                  style={{ top: "35px", left: "10px" }}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  aria-hidden="true"
+                  data-reactid="1036"
+                >
                   <line x1="22" y1="2" x2="11" y2="13"></line>
                   <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
                 </svg>
               </a>
-
             </div>
           </div>
-        }
-        {showChatbox &&
-          <div className="form-popup" style={{ display: chatForm ? "none" : "block" }}>
-            <h2 className="h23" >Nhập tên của bạn để bắt đầu trò chuyện: </h2>
-            <input style={{ position: "relative" }} class="chat__conversation-panel__input panel-item" placeholder="Nhập tên của bạn..." onKeyDown={(event) => {
-              if (event.keyCode == 13) {
-                handleStartChat();
-              }
-            }}
+        )}
+        {showChatbox && (
+          <div
+            className="form-popup"
+            style={{ display: chatForm ? "none" : "block" }}
+          >
+            <h2 className="h23">Nhập tên của bạn để bắt đầu trò chuyện: </h2>
+            <input
+              style={{ position: "relative" }}
+              class="chat__conversation-panel__input panel-item"
+              placeholder="Nhập tên của bạn..."
+              onKeyDown={(event) => {
+                if (event.keyCode == 13) {
+                  handleStartChat();
+                }
+              }}
               type="text"
               value={username}
-              onChange={(e) => setUsername(e.target.value)} />
-            <button className="chat__conversation-panel__button panel-item btn-icon send-message-button" onClick={handleStartChat}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" data-reactid="1036">
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <button
+              className="chat__conversation-panel__button panel-item btn-icon send-message-button"
+              onClick={handleStartChat}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                aria-hidden="true"
+                data-reactid="1036"
+              >
                 <line x1="22" y1="2" x2="11" y2="13"></line>
                 <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
               </svg>
             </button>
             {/* <button onClick={handleStartChat}>  <i className="fa-solid fa-check"></i></button> */}
           </div>
-        }
+        )}
         {/* popup */}
         <div>
-          {chatForm == false ?
+          {chatForm == false ? (
             <button
               id="fpt_ai_livechat_button"
               className="fpt_ai_livechat_button_blink"
               style={{ background: "#EEEEEEEE" }}
               onClick={() => {
-                setShowChatbox(!showChatbox)
-                setShowbutton(!showButton)
+                setShowChatbox(!showChatbox);
+                setShowbutton(!showButton);
                 console.log(showButton);
-              }} >
+              }}
+            >
               <img
                 src="https://cdn-static-v3.fpt.ai/upload/cc5d89ad1a2ac3ef4db45d7acf2d1ed6/61b5ac99f8e432b71a75e90e9aa27963.png"
                 alt="logobutton"
               />
             </button>
-            :
+          ) : (
             <button
               id="fpt_ai_livechat_button"
               className="fpt_ai_livechat_button_blink"
               style={{ background: "#EEEEEEEE" }}
               onClick={() => {
-                setChatForm(!chatForm)
-                setShowChatbox(!showChatbox)
+                setChatForm(!chatForm);
+                setShowChatbox(!showChatbox);
                 // setShowbutton(showButton)
-              }} >
-              <img src="https://img.icons8.com/external-bluetone-bomsymbols-/91/external-close-digital-design-bluetone-set-2-bluetone-bomsymbols-.png" alt="external-close-digital-design-bluetone-set-2-bluetone-bomsymbols-" />
+              }}
+            >
+              <img
+                src="https://img.icons8.com/external-bluetone-bomsymbols-/91/external-close-digital-design-bluetone-set-2-bluetone-bomsymbols-.png"
+                alt="external-close-digital-design-bluetone-set-2-bluetone-bomsymbols-"
+              />
             </button>
-          }
+          )}
         </div>
       </div>
     </>
